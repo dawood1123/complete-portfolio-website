10fa86db958704484b9b199472e10f5f2fe59329
dawood1123 <davidkhan1226@gmail.com>
2024-10-05 21:41:41 -0700

    added

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const hamburger = document.querySelector(".hamburger")
-
-hamburger.addEventListener("click", () => { 
-    hamburger.classList.toggle("active");
-})
+function showhamburger(){ 
+    var hamburger = document.querySelector('.hamburger'); 
+    hamburger.style.display = 'flex'
+}
+    
+function closehamburger(){ 
+    var hamburger = document.querySelector('.hamburger'); 
+    hamburger.style.display = 'none'
+}
